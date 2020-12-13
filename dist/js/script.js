fe6fb3fbe9b517d6bc012e6e63c14edbc4347e2e
joshysmart <sanijoshua10@gmail.com>
2020-12-13 15:04:43 +0100

    completed frontend challenge

--- dist/js/script.js
+++ dist/js/script.js
@@ -144,9 +144,6 @@ Todo App Init Items
 
 function initItems() {
  const todoItems = todoWrapper.querySelectorAll('.todo-item');
- const todoItemsStorage = [...todoItems] 
- console.log(todoItemsStorage);
- localStorage.setItem('items', JSON.stringify(todoItemsStorage));
  const itemsCount = document.querySelector('.items-count');
  const closeButtons = todoWrapper.querySelectorAll('.close');
  
